419fd2bdcbf386464021c78637d42a3e277f5930
Priyanka Malviya <priyanka_malviya@icloud.com>
2016-10-15 01:30:34 -0700

    create notes

--- app/components/Notes/Notes.js
+++ app/components/Notes/Notes.js
@@ -1,11 +1,13 @@
 var React= require('react');
+var NotesList = require('./NotesList');
 
 var Notes = React.createClass({
   render: function(){
+    console.log('Notes', this.props.notes);
     return(
       <div>
-      <p>NOTES</p>
-      <p>{this.props.notes}</p>
+      <h3>Notes for {this.props.username}</h3>
+        <NotesList notes={this.props.notes}/>
       </div>
     )
   }
