e91fc9318eac508a580d8d43eb04d0acba1dbf50
Priyanka Malviya <priyanka_malviya@icloud.com>
2018-07-11 11:00:31 -0700

    removed dead code

--- app/App.js
+++ app/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router';
 import routes from './config/routes';
-//import { createHistory } from 'history';
 
 ReactDOM.render(<Router >{routes}</Router>,
   document.getElementById('app')
